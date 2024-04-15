cfe5e8cc85790bef1d63ba6250a87f1701060bed
Brendan Ward <brendan.ward@host-net.org>
2024-04-14 18:37:55 -0600

    Created initial code for file list
    - Used a recyclerview to show a list of files
    - Initial implementation as a work in progress
    - Next step is to obtain the list of files from the file system

--- app/src/main/java/org/brendanpward/keypad/ui/home/HomeFragment.java
+++ app/src/main/java/org/brendanpward/keypad/ui/home/HomeFragment.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import org.brendanpward.keypad.R;
 import org.brendanpward.keypad.databinding.FragmentHomeBinding;
+import java.util.ArrayList;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements FileMenuAdapter.ItemClickListener {
 
     private FragmentHomeBinding binding;
+    FileMenuAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +29,24 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //final TextView textView = binding.textHome;
-        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Populate the RecyclerView
+        ArrayList<String> ListOfFiles = new ArrayList<>();
+        ListOfFiles.add("File1");
+        ListOfFiles.add("File2");
+
+        RecyclerView recyclerView = root.findViewById(R.id.fileList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        adapter = new FileMenuAdapter(root.getContext(), ListOfFiles);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
         return root;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        //Check this line
+        Toast.makeText(getActivity().getApplicationContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -34,4 +54,5 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
